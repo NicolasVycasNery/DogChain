50ab0f64d88fedb990748b8aaf8ad58c431a3930
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-13 20:56:35 -0300

    Add context providers to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Web3Provider } from "./context/web3Context.jsx";
+import { DogContractProvider } from "./context/DogContractContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Web3Provider>
+            <DogContractProvider>
+                <App />
+            </DogContractProvider>
+        </Web3Provider>
+
+    </React.StrictMode>,
 )
