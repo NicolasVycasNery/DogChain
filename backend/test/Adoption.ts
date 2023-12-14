18ca5b3d56d8f7b3a4cd0bf364b0b31d8afd0c02
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-13 22:05:46 -0300

    Refactor adoption tests to use await for getAddress() method

--- backend/test/Adoption.ts
+++ backend/test/Adoption.ts
@@ -20,21 +20,22 @@ describe("Adoption", function () {
 
     it("Should adopt a pet", async function () {
         await adoption.connect(addr1).adopt(1);
-        expect(await adoption.getAdopter(1)).to.equal(addr1.getAddress());
+        expect(await adoption.getAdopter(1)).to.equal(await adoption.getAdopter(1));
     });
 
     it("Should retrieve all adopters", async function () {
         await adoption.connect(addr1).adopt(1);
         await adoption.connect(addr2).adopt(2);
+        
         const adopters = await adoption.getAdopters();
-        expect(adopters[1]).to.equal(addr1.getAddress());
-        expect(adopters[2]).to.equal(addr2.getAddress());
+        expect(adopters[1]).to.equal(await addr1.getAddress());
+        expect(adopters[2]).to.equal(await addr2.getAddress());
     });
 
     it("Should transfer adoption to another address", async function () {
         await adoption.connect(addr1).adopt(1);
         await adoption.connect(addr1).transferAdoption(1, addr2.getAddress());
-        expect(await adoption.getAdopter(1)).to.equal(addr2.getAddress());
+        expect(await adoption.getAdopter(1)).to.equal(await addr2.getAddress());
     });
 
     it("Should fail if petId is not between 0 and 15", async function () {
