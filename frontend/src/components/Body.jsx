d73413da28b76cda0190edc890889df81f2dc501
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-14 20:40:15 -0300

    Refactor Body component to use useWeb3 hook and display loading state

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,58 +1,94 @@
 import { useEffect, useState } from 'react';
-import { useDogContract } from '../context/DogContractContext';
+import Swal from 'sweetalert2';
 import dogsData from '../data/dogs.js';
+import { useWeb3 } from '../hooks';
 import DogCard from './DogCard';
+import { ethers } from 'ethers';
 
 
 function Body() {
     const [dogs, setDogs] = useState(dogsData);
-    const { getAdopters } = useDogContract();
-    const [adopters, setAdopters] = useState([]);
+    const { getAdopters, error } = useWeb3();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // update the state of the dog adoption list
         async function load() {
             const adopters = await getAdopters();
-            console.log(adopters);
-            setAdopters(adopters);
+            if (!adopters)
+                return;
             const dogs = dogsData.map((dog, i) => {
-                return { ...dog, adopter: adopters[i], adopted: adopters[i] != null };
+                const adopter = adopters[i];
+                const adopted = adopters[i] != "0x0000000000000000000000000000000000000000"
+                return {
+                    ...dog,
+                    adopter: adopter,
+                    adopted: adopted
+                };
             });
             setDogs(dogs);
+            setLoading(false);
         }
         load();
-    }, [getAdopters]);
+    }, []);
+
+    useEffect(() => {
+        if (error)
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: error,
+            });
+    }, [error]);
+
+
 
     return (
         <>
-            <section className="p-6">
+            <section className="p-6 min-h-screen bg-gray-200">
                 <h1 className="text-4xl font-bold mb-4">
                     All Dogs
                 </h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {dogs.map((dog, i) => {
-                        return (
-                            <DogCard
-                                dog={dog}
-                                key={i}
-                            />
-                        )
-                    })}
+                    {
+                        !loading ?
+                            dogs.map((dog, i) => {
+                                return (
+                                    <DogCard
+                                        dog={dog}
+                                        key={i}
+                                    />
+                                )
+                            })
+                            :
+                            <div className="text-2xl font-bold text-center">
+                                Loading...
+                            </div>
+                    }
                 </div>
             </section>
-            <section>
+            <section className="p-6 min-h-screen bg-gray-200">
                 <h1 className="text-4xl font-bold mb-4">
                     Your Dogs
                 </h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {dogs.filter(dog => dog.adopted).map((dog, i) => {
-                        return (
-                            <DogCard
-                                dog={dog}
-                                key={i}
-                            />
-                        )
-                    })}
+                    {
+                        !loading ?
+                            dogs.filter(dog => {
+                                return ethers.getAddress(dog.adopter) === ethers.getAddress(window.ethereum.selectedAddress);
+                            }).map((dog, i) => {
+                                return (
+                                    <DogCard
+                                        dog={dog}
+                                        key={i}
+                                        isOwner={true}
+                                    />
+                                )
+                            }) :
+                            <div className="text-2xl font-bold text-center">
+                                Loading...
+                            </div>
+                    }
                 </div>
             </section>
         </>
