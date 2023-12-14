5277aeb2d32f9ea5a4481a9a2b0ac3e048b29f97
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-14 20:40:30 -0300

    Add new dog images to the dogs.js file

--- frontend/src/data/dogs.js
+++ frontend/src/data/dogs.js
@@ -1,108 +1,99 @@
 const dogs = [
     {
+        id: 0,
+        name: "Joost",
+        age: 15,
+        img: "/imgs/dog0.jpg"
+    },
+    {
         id: 1,
         name: "Rex",
         age: 5,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog1.jpg"
     },
     {
         id: 2,
         name: "Buddy",
         age: 2,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog2.jpg"
     },
     {
         id: 3,
         name: "Luna",
         age: 3,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog3.jpg"
     },
     {
         id: 4,
         name: "Cooper",
         age: 4,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog4.jpg"
     },
     {
         id: 5,
         name: "Daisy",
         age: 1,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog5.jpg"
     },
     {
         id: 6,
         name: "Bailey",
         age: 2,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog6.jpg"
     },
     {
         id: 7,
         name: "Lola",
         age: 3,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog7.jpg"
     },
     {
         id: 8,
         name: "Frankie",
         age: 4,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog8.jpg"
     },
     {
         id: 9,
         name: "Lily",
         age: 1,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog9.jpg"
     },
     {
         id: 10,
         name: "Belinha",
         age: 2,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog10.jpg"
     },
     {
         id: 11,
         name: "Bella",
         age: 3,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog11.jpg"
     },
     {
         id: 12,
         name: "Rex",
         age: 4,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog12.jpg"
     },
     {
         id: 13,
         name: "Zeus",
         age: 5,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog13.jpg"
     },
     {
         id: 14,
         name: "Lua",
         age: 1,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog14.jpg"
     },
     {
         id: 15,
         name: "Friday",
         age: 2,
-        img: "https://dog.ceo/api/breeds/image/random",
-        adopted: false,
+        img: "/imgs/dog15.jpg"
     },
 ];
 
