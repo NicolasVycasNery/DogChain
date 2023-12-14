82886e9c6e800685cef4260eeec061caa3b3fc4a
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-13 22:13:33 -0300

    Add error handling and additional contract functions

--- frontend/src/context/DogContractContext.jsx
+++ frontend/src/context/DogContractContext.jsx
@@ -12,8 +12,62 @@ export const DogContractProvider = ({
 }) => {
     const { signer } = useContext(Web3Context);
     const [contract, setContract] = useState(null);
+    const [error, setError] = useState(null);
 
-    const load = async () => {
+    function checkContract() {
+        if (!contract) {
+            return false;
+        }
+        return true;
+    }
+
+    async function adopt(id) {
+        if (checkContract()) {
+            return;
+        }
+        try {
+            const tx = await contract.adopt(id);
+            await tx.wait();
+        } catch (e) {
+            setError(e.message);
+        }
+
+    }
+    async function getAdopters() {
+        if (checkContract()) {
+            return;
+        }
+        try {
+            const adopters = await contract.getAdopters();
+            return adopters;
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+    async function getAdopter(id) {
+        if (checkContract()) {
+            return;
+        }
+        try {
+            const adopter = await contract.getAdopter(id);
+            return adopter;
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+    async function transferAdoption(id, to_address) {
+        if (checkContract()) {
+            return;
+        }
+        try {
+            const tx = await contract.transferAdoption(id, to_address);
+            await tx.wait();
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+
+    async function load() {
         const abi = contractABI.abi;
         if (!signer) {
             console.error("Signer not loaded");
@@ -30,18 +84,22 @@ export const DogContractProvider = ({
         // Create a new ethers.js instance
         const contract = new ethers.Contract(ContractAddress, abi, signer);
         setContract(contract);
-    };
+    }
 
     useEffect(() => {
         if (signer) {
             load();
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [signer]);
 
     return (
         <DogContractContext.Provider value={{
-            contract,
+            error,
+            adopt,
+            getAdopters,
+            getAdopter,
+            transferAdoption,
         }}>
             {children}
         </DogContractContext.Provider>
